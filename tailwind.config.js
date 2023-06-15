8b00378a936b9dac1adea43bbe92f9d07a309e96
effervescent000 <kislio01@gmail.com>
2023-06-15 16:38:53 -0400

    set up tailwind basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			'light-gray': '#d6d9d3',
+			'dark-purple': '#2f2235',
+			'medium-purple': '#3f3244',
+			'light-purple': '#60495a',
+			'medium-gray': '#a9aca9'
+		}
 	},
 	plugins: []
 };
