90b892c770d894bfba622caf7b1d09cbcdac87f1
effervescent000 <kislio01@gmail.com>
2023-06-23 08:27:40 -0400

    create form

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -4,4 +4,21 @@
 	// STATE
 
 	// LOGIC
+
+	const settingsList = [
+		{ key: 'growingSeason', label: 'Length of growing season in days' },
+		{ key: 'pctNutritionFromGrowing', label: '% of food from growing' }
+	];
 </script>
+
+<form method="POST">
+	<div class="flex gap-10">
+		{#each settingsList as setting}
+			<label class="grid grid-cols-2 gap-2">
+				<span>{setting.label}</span>
+				<input name={setting.key} autocomplete="off" />
+			</label>
+		{/each}
+	</div>
+	<button type="submit">Save</button>
+</form>
